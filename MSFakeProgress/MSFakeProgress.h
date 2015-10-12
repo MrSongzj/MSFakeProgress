21eea7ad0ea10c3768aee5eb74c1648eb2ab9133
MrSong <424607870@qq.com>
2015-10-12 14:54:36 +0800

    添加注释

--- MSFakeProgress/MSFakeProgress.h
+++ MSFakeProgress/MSFakeProgress.h
@@ -8,15 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
-#define timerInterval 0.005
-#define progressIncrement 0.0025f
+#define timerInterval 0.005             // 定时器间隔时间
+#define progressIncrement 0.0025f       // 进度条的增量
 
 @protocol MSFakeProgressDelegate <NSObject>
 
+@optional
+
+// 进度开始时回调的方法
 - (void)fakeProgressStart;
-
+// 进度走动中回调的方法
 - (void)fakeProgressRunning:(float)progress;
-
+// 进度完成时回调的方法
 - (void)fakeProgressFinish;
 
 @end
@@ -26,11 +29,11 @@
 @property (weak, nonatomic) id<MSFakeProgressDelegate> fakeProgressDelegate;
 
 - (instancetype)initWithDelegate:(id<MSFakeProgressDelegate>)delegate;
-
+// 重置进度
 - (void)reset;
-
+// 进度开始走动
 - (void)start;
-
+// 进度完成
 - (void)finish;
 
 @end
